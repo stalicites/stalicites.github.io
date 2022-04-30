66abfc30b7b46204a8611fdd567f63ac856b0ad4
Stalicites <88872425+stalicites@users.noreply.github.com>
2022-04-30 08:46:57 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+VANTA.GLOBE({
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: "#00ff00",
+    color2: "white",
+    size: 1.6,
+    backgroundColor: "#171717"
+  })
+  
+  const header = document.getElementById("header");
+  const headerText = "STALICITES";
+  let index = 0;
+  
+  setInterval(function() {
+    if (index < headerText.length) {
+      header.innerHTML = header.innerHTML + headerText[index];
+      index++;
+    } 
+  }, 1000/10);  
