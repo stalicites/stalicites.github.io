00a57c09f24854e5cac16cc350b98d5335e02567
Stalicites <88872425+stalicites@users.noreply.github.com>
2022-06-11 10:32:33 -0700

    Delete deesquirrel directory

--- deesquirrel/settings.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("save").onclick = function() {
-    console.log("Saving Changes...");
-    localStorage.setItem("p1", document.getElementById("p1").value);
-    localStorage.setItem("p2", document.getElementById("p2").value);
-    localStorage.setItem("board", document.getElementById("board").value);
-    window.location = "index.html"
-}
