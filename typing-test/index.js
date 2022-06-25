d5f41eddc0b5b3bd66c1be58028a389458c876a3
Stalicites <88872425+stalicites@users.noreply.github.com>
2022-06-24 17:17:58 -0700

    Delete typing-test directory

--- typing-test/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("go").onclick = function() {
-    window.location = "test.html?words=" + document.getElementById("word-count").value;
-}
-
-function update(val)
-{
-    document.getElementById("wc").innerHTML = "Word Count: " + val;
-} 
